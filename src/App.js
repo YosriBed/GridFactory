9047f33c1442a2e8dcf4025e1507bdaea12ecb06
Yosri Bedoui <yosri.bedoui@pictureworks.com.au>
2021-07-07 21:09:21 +0100

    feat: graph generation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import domtoimage from "dom-to-image";
+import "./App.css";
+import Square from "./Square";
+import { generateAndDownloadBlob } from "./utils";
 
 function App() {
+  const [numberOfSquares, setNumberOfSquares] = useState(5);
+  const [grid, setGrid] = useState([]);
+  useEffect(() => {
+    let squares = [];
+    for (let i = 0; i < numberOfSquares; i++) {
+      squares.push(<Square i={i} />);
+    }
+    setGrid(squares);
+  }, [numberOfSquares]);
+
+  const changeNumber = (e) => {
+    const UPPER_LIMIT = 10000;
+    const LOWER_LIMIT = 0;
+    if (e.target.value <= UPPER_LIMIT && e.target.value >= LOWER_LIMIT)
+      setNumberOfSquares(e.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <input
+        onChange={changeNumber}
+        value={numberOfSquares}
+        type='number'
+        placeholder='Number of elements...'
+      />
+      <div className='header'>
+        <div>
+          <div>GRAPH</div>
+          <div>For {numberOfSquares} elements</div>
+          <button
+            type='button'
+            onClick={() => {
+              domtoimage
+                .toBlob(document.getElementById("grid"))
+                .then(generateAndDownloadBlob)
+                .catch((err) => console.log(err));
+            }}
+          >
+            DOWNLOAD
+          </button>
+        </div>
+      </div>
+      <div id='grid' className='grid'>
+        {grid}
+      </div>
     </div>
   );
 }
